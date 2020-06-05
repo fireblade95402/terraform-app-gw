c6489e466159f286dee8e382091eec493a432223
Mark Graham <magraham@microsoft.com>
2020-06-05 11:26:34 +0100

    clean up files

--- examples/front-door-plus-WAF/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "front-door-object" {
-  value = module.front-door.front-door-object
-}
-
-output "front-door-waf-object" {
-  value = module.front-door.front-door-waf-object
-}
-
--- examples/front-door-plus-WAF/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "front-door-object" {
-  description = "(Required) Front Door Object configuration"  
-}
-
-variable "front-door-waf-object" {
-    description = "(Required) Front Door WAF Object configuration"
-}
-
-
